e6355fe557be659e7e7ed46df9165145a73dbafe
etfaghaoubeid <etfaghaoubeid@gmail.com>
2019-04-30 12:11:13 +0000

    custom card description

--- src/components/movie/Movie.js
+++ src/components/movie/Movie.js
@@ -2,12 +2,12 @@ import React from 'react'
 import { Card, Button, CardImg, CardTitle, CardText, CardDeck,
   CardSubtitle, CardBody,CardImgOverlay } from 'reactstrap'
 
-function Movie({movie:{poster_path,title,overview}}) {
+function Movie({movie:{poster_path,title,overview,release_date}}) {
   //console.log(movie)
   let path = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2'
   return (
   
-     <div className="col-lg-5  card-item card my-3 mx-3" >
+     <div className="col-lg-5  card-item card my-2 mx-3" >
   <div className="row no-gutters">
     <div className="col-md-4">
       <img className="card-item-img" src={path+poster_path} className="card-img" alt="..."/>
@@ -15,11 +15,15 @@ function Movie({movie:{poster_path,title,overview}}) {
     <div className="col-md-8">
       <div className="card-body">
       <div className="card-item-header">
-        <h5 className="card-title card-item-title"><strong>{title}</strong></h5>
-        <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+        <p className="card-title "><strong className="card-item-title">{title}</strong>  <br/>
+        <span className="card-item-date">{release_date}</span>
+         </p>
+        
+        
       </div>
-        <p className="card-text card-item-text">{overview}</p>
-        <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+        <p className="card-text card-item-text">{overview.substr(0,90)}...</p>
+        <hr></hr>
+        <div class="card-item-more-info">More info</div>
       </div>
     </div>
   </div>
